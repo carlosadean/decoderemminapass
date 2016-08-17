f37f31d54a4686976f616fdde62768f7db1c241c
Carlos Adean <carlosadean@gmail.com>
2016-08-17 16:29:57 -0300

    Create decode.py
    Decoding remmina password

---
+++ decode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# 
+
+from Crypto.Cipher import DES3
+import base64
+
+secret = base64.decodestring('PASTE THE SECRET FOUND AT POINT 1')
+key = secret[:24]
+iv = secret[24:]
+
+# Encoded Encrypted password
+EEpwd = 'PASTE THE PASSWORD FOUND AT POINT 2'
+
+# Decoded Encrypted password
+DEpwd = base64.decodestring(EEpwd)
+
+# Decoded Decrypted password
+DDpwd = DES3.new(key, DES3.MODE_CBC, iv).decrypt(DEpwd)
+print "Decoded (Decrypted ( PWD ) ) : ",DDpwd
